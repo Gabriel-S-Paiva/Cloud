3aa3c3a52f8567df7aa0518bebe430961c050e14
Gabriel-S-Paiva <mr.sousapaiva@gmail.com>
2026-07-08 17:17:49 +0100

    Initial Database Schema

---
+++ migration/001_init.sql
@@ -0,0 +1,51 @@
+CREATE TABLE Requests (
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    hashed_password TEXT NOT NULL,
+    status TEXT NOT NULL CHECK (status IN ('Pending', 'Rejected')) DEFAULT 'Pending'
+);
+
+CREATE TABLE Users (
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    hashed_password TEXT NOT NULL,
+    role TEXT NOT NULL CHECK (role IN ('User', 'Admin')) DEFAULT 'User',
+    quota INTEGER NOT NULL,
+    quota_used INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE TABLE Folders (
+    id INTEGER PRIMARY KEY,
+    display_name TEXT NOT NULL,
+    owned_by INTEGER NOT NULL,
+    parent_folder INTEGER,
+
+    FOREIGN KEY (owned_by) REFERENCES Users(id),
+    FOREIGN KEY (parent_folder) REFERENCES Folders(id)
+);
+
+CREATE TABLE Files (
+    id INTEGER PRIMARY KEY,
+    display_name TEXT NOT NULL,
+    owned_by INTEGER NOT NULL,
+    size INTEGER NOT NULL,
+    uploaded_at INTEGER NOT NULL DEFAULT (unixepoch()),
+    last_modified INTEGER NOT NULL DEFAULT (unixepoch()),
+    parent_folder INTEGER,
+
+    FOREIGN KEY (owned_by) REFERENCES Users(id),
+    FOREIGN KEY (parent_folder) REFERENCES Folders(id)
+);
+
+CREATE TABLE Shares (
+    file INTEGER,
+    folder INTEGER,
+    shared_with INTEGER NOT NULL,
+    permissions TEXT NOT NULL CHECK(permissions IN ('View','Edit')),
+
+    CHECK ((file IS NOT NULL AND folder IS NULL) OR (file IS NULL AND folder IS NOT NULL)),
+    PRIMARY KEY (file, folder, shared_with),
+    FOREIGN KEY (file) REFERENCES Files(id),
+    FOREIGN KEY (folder) REFERENCES Folders(id),
+    FOREIGN KEY (shared_with) REFERENCES Users(id)
+);
