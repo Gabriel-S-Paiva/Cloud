e9b06abcc75837d3d21ee1a9b8c9d5fe950a1712
Gabriel-S-Paiva <mr.sousapaiva@gmail.com>
2026-07-18 02:11:07 +0100

    Shares Storage implemented. Handler is a problem from tomorrow's me. . . well 5 hours me

---
+++ backend/storage/shares.go
@@ -0,0 +1,137 @@
+package storage
+
+import (
+	"context"
+	"database/sql"
+)
+
+type Share struct {
+	Id         int
+	File       sql.NullInt64
+	Folder     sql.NullInt64
+	SharedWith int
+	Permission string
+}
+
+func (s *Store) CreateShare(ctx context.Context, file sql.NullInt64, folder sql.NullInt64, sharedWith int, permission string) error {
+	if _, err := s.db.ExecContext(ctx, "INSERT INTO Shares (file, folder, shared_with, permissions) VALUES (?,?,?,?)", file, folder, sharedWith, permission); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Shared With Me
+func (s *Store) GetIncomingShares(ctx context.Context, userId int) (*FolderContents, error) {
+	var folderContents FolderContents
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	fileRows, err := tx.QueryContext(ctx, "SELECT id, display_name, owned_by, size, uploaded_at, last_modified, parent_folder FROM Files AS f JOIN Shares AS s ON f.id = s.file WHERE s.shared_with = ?", userId)
+	if err != nil {
+		return nil, err
+	}
+	defer fileRows.Close()
+	for fileRows.Next() {
+		var file File
+		if err := fileRows.Scan(&file.Id, &file.DisplayName, &file.OwnedBy, &file.Size, &file.UploadedAt, &file.LastModified, &file.ParentFolder); err != nil {
+			return nil, err
+		}
+		folderContents.Files = append(folderContents.Files, file)
+	}
+	if err := fileRows.Err(); err != nil {
+		return nil, err
+	}
+
+	folderRows, err := tx.QueryContext(ctx, "SELECT id, display_name, owned_by, parent_folder FROM Folders AS f JOIN Shares AS s ON f.id = s.folder WHERE s.shared_with = ?", userId)
+	if err != nil {
+		return nil, err
+	}
+	defer folderRows.Close()
+	for folderRows.Next() {
+		var folder Folder
+		if err := folderRows.Scan(&folder.Id, &folder.DisplayName, &folder.OwnedBy, &folder.ParentFolder); err != nil {
+			return nil, err
+		}
+		folderContents.Folders = append(folderContents.Folders, folder)
+	}
+	if err := folderRows.Err(); err != nil {
+		return nil, err
+	}
+	return &folderContents, nil
+}
+
+// Shared Files
+func (s *Store) GetOutgoingShares(ctx context.Context, userId int) (*FolderContents, error) {
+	var folderContents FolderContents
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	fileRows, err := tx.QueryContext(ctx, "SELECT id, display_name, owned_by, size, uploaded_at, last_modified, parent_folder FROM Files AS f JOIN Shares AS s ON f.id = s.file WHERE f.owned_by = ?", userId)
+	if err != nil {
+		return nil, err
+	}
+	defer fileRows.Close()
+	for fileRows.Next() {
+		var file File
+		if err := fileRows.Scan(&file.Id, &file.DisplayName, &file.OwnedBy, &file.Size, &file.UploadedAt, &file.LastModified, &file.ParentFolder); err != nil {
+			return nil, err
+		}
+		folderContents.Files = append(folderContents.Files, file)
+	}
+	if err := fileRows.Err(); err != nil {
+		return nil, err
+	}
+
+	folderRows, err := tx.QueryContext(ctx, "SELECT id, display_name, owned_by, parent_folder FROM Folders AS f JOIN Shares AS s ON f.id = s.folder WHERE f.owned_by = ?", userId)
+	if err != nil {
+		return nil, err
+	}
+	defer folderRows.Close()
+	for folderRows.Next() {
+		var folder Folder
+		if err := folderRows.Scan(&folder.Id, &folder.DisplayName, &folder.OwnedBy, &folder.ParentFolder); err != nil {
+			return nil, err
+		}
+		folderContents.Folders = append(folderContents.Folders, folder)
+	}
+	if err := folderRows.Err(); err != nil {
+		return nil, err
+	}
+	return &folderContents, nil
+}
+
+func (s *Store) UpdatePermission(ctx context.Context, shareId int, newPermission string) error {
+	res, err := s.db.ExecContext(ctx, "UPDATE Shares SET permissions = ? WHERE id = ?", newPermission, shareId)
+	if err != nil {
+		return err
+	}
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rows != 1 {
+		return ErrUpdatingShare
+	}
+	return nil
+}
+
+func (s *Store) DeleteShare(ctx context.Context, id int) error {
+	result, err := s.db.ExecContext(ctx, "DELETE FROM Shares WHERE id = ?", id)
+	if err != nil {
+		return err
+	}
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rows == 0 {
+		return ErrShareNotFound
+	}
+	return nil
+}
