c669ab539d7438dca638b4468f5ecfb64bf69cdd
Gabriel-S-Paiva <mr.sousapaiva@gmail.com>
2026-08-07 10:26:29 +0100

    Fix style refresh issue

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-    import '../app.css'
-    import { goto } from "$app/navigation";
+	import { goto } from "$app/navigation";
 	import { onMount } from "svelte";
 
-    onMount(() => goto('/login'))
+    onMount(()=>goto('/login'))
 </script>
